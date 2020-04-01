d96170f8696ba82c0efe7345c8c8010e8592701a
Kossa123 <dominik.kossinski@akai.org.pl>
2020-04-01 21:52:17 +0200

    init commit

---
+++ src/main/java/com/example/SpringExampleJava/SpringExampleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringExampleJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringExampleJava/SpringExampleJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringExampleJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
